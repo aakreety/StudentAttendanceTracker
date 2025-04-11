a3d20ba14d7db0b22fdc33e7d0545a0d213608a8
aakreety <aakritin3@gmail.com>
2025-04-10 23:57:10 -0500

    initializing app with main window and title

---
+++ app.py
@@ -0,0 +1,12 @@
+# gui/app.py
+
+import tkinter as tk
+
+def main():
+    root = tk.Tk()
+    root.title("Student Attendance Tracker")
+    root.geometry("600x400")
+    root.mainloop()
+
+if __name__ == "__main__":
+    main()
